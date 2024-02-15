f2bf98026b57666f3739eae8bbcb4a117e13b4bb
igordev97 <business.igorstojadinovic97@gmail.com>
2024-02-15 21:26:17 +0100

    final version beta 1.0.0

--- bazaCopy/webshop.sql
+++ bazaCopy/webshop.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Feb 11, 2024 at 10:30 PM
+-- Generation Time: Feb 15, 2024 at 09:25 PM
 -- Server version: 10.4.32-MariaDB
 -- PHP Version: 8.2.12
 
@@ -24,16 +24,55 @@ SET time_zone = "+00:00";
 -- --------------------------------------------------------
 
 --
--- Table structure for table `products`
+-- Table structure for table `korpa`
 --
 
-CREATE TABLE `products` (
+CREATE TABLE `korpa` (
   `id` int(11) NOT NULL,
-  `product_name` varchar(64) NOT NULL,
-  `product_description` text NOT NULL,
-  `product_price` int(11) NOT NULL,
-  `product_img_url` varchar(128) NOT NULL
+  `id_korisnik` int(11) NOT NULL,
+  `id_proizvod` int(11) NOT NULL,
+  `ime_proizvod` varchar(64) NOT NULL,
+  `kolicina_proizvod` int(11) NOT NULL,
+  `cena_proizvod` int(11) NOT NULL,
+  `slika_proizvod` varchar(128) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `korpa`
+--
+
+INSERT INTO `korpa` (`id`, `id_korisnik`, `id_proizvod`, `ime_proizvod`, `kolicina_proizvod`, `cena_proizvod`, `slika_proizvod`) VALUES
+(102, 6, 15, 'Kingston DIMM DDR4 16GB 3200MHz', 1, 8500, 'kingston-dimm-ddr4-16gb-3200mhz-kf432c16bb1-16-fury-beast-ram-memorija-cene.jpg'),
+(103, 7, 13, 'Asus Prime B550A-CMS Maticna Ploca', 1, 13000, 'asus-prime-b550m-a-maticna-ploca-6540ea0c7e986.jpeg'),
+(104, 7, 12, 'AMD Ryzen 5 5600x -Box Kuler', 1, 19000, 'ryzen.png'),
+(105, 7, 14, 'AMD RX 6650 XT 8GB DDR6 - Power Color', 1, 31000, 'image64ec8d7ae5881.jpg'),
+(106, 7, 15, 'Kingston DIMM DDR4 16GB 3200MHz', 1, 8500, 'kingston-dimm-ddr4-16gb-3200mhz-kf432c16bb1-16-fury-beast-ram-memorija-cene.jpg');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `proizvodi`
+--
+
+CREATE TABLE `proizvodi` (
+  `id` int(11) NOT NULL,
+  `ime_proizvoda` varchar(64) NOT NULL,
+  `opis_proizvoda` text NOT NULL,
+  `cena_proizvoda` int(11) NOT NULL,
+  `kolicina_proizvoda` int(11) NOT NULL,
+  `kategorija_proizvoda` varchar(42) NOT NULL,
+  `slika_proizvoda` varchar(128) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `proizvodi`
+--
+
+INSERT INTO `proizvodi` (`id`, `ime_proizvoda`, `opis_proizvoda`, `cena_proizvoda`, `kolicina_proizvoda`, `kategorija_proizvoda`, `slika_proizvoda`) VALUES
+(12, 'AMD Ryzen 5 5600x -Box Kuler', 'Procesor: AMD Ryzen 5 5600x\r\nSoket:AM4\r\n\r\nSavrsen gaming procesor spreman za igranjae', 19000, 99, 'procesori', 'ryzen.png'),
+(13, 'Asus Prime B550A-CMS Maticna Ploca', 'Soket:AM4\r\nRam Support : DDR4\r\nRGB:yes', 13000, 100, 'maticne_ploce', 'asus-prime-b550m-a-maticna-ploca-6540ea0c7e986.jpeg'),
+(14, 'AMD RX 6650 XT 8GB DDR6 - Power Color', 'Savrsena grafika za sve moderne igre.\r\n\r\nSa ovom grafikom mozete igrati sve najnovije naslove na max detaljima u FULL HD rezoluciji', 31000, 100, 'graficke', 'image64ec8d7ae5881.jpg'),
+(15, 'Kingston DIMM DDR4 16GB 3200MHz', '16GB DDR4 brza memorija sve sto vam je potrebno za vase ugodno koriscenje', 8500, 100, 'ram_memorije', 'kingston-dimm-ddr4-16gb-3200mhz-kf432c16bb1-16-fury-beast-ram-memorija-cene.jpg');
 
 -- --------------------------------------------------------
 
@@ -56,16 +95,23 @@ CREATE TABLE `users` (
 INSERT INTO `users` (`id`, `korisnicko_ime`, `ime`, `email`, `lozinka`) VALUES
 (4, 'kcblaze', 'Igor', 'igor@gmail.com', '$2y$10$JgHxox72Fj1VkvELHPWrO.zC.omWVhxECwW88ZZRqOZam0r/2auNu'),
 (5, 'kcblaze97', 'Igor', 'igor123@gmail.com', '$2y$10$n7KD/GVTi1vsvKvAXS78f.gQhZRQqaRP2z03Y2nMNo83wbbu6d9HS'),
-(6, 'admin', 'admin', 'admin@gmail.com', '$2y$10$bDkhxys8SSts784kNGCJ/eMx1wNBvCmxwC4OcE.3kr9Oyjjys5nTm');
+(6, 'admin', 'admin', 'admin@gmail.com', '$2y$10$bDkhxys8SSts784kNGCJ/eMx1wNBvCmxwC4OcE.3kr9Oyjjys5nTm'),
+(7, 'igor123', 'Igor Stojadinovic', 'igorstojadinovic977@gmail.com', '$2y$10$5UZiv8/0aHtmUhTLiiKNkuVW7gai9Ydnt2zHbaOyuuaavH8ITwviO');
 
 --
 -- Indexes for dumped tables
 --
 
 --
--- Indexes for table `products`
+-- Indexes for table `korpa`
 --
-ALTER TABLE `products`
+ALTER TABLE `korpa`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `proizvodi`
+--
+ALTER TABLE `proizvodi`
   ADD PRIMARY KEY (`id`);
 
 --
@@ -81,16 +127,22 @@ ALTER TABLE `users`
 --
 
 --
--- AUTO_INCREMENT for table `products`
+-- AUTO_INCREMENT for table `korpa`
 --
-ALTER TABLE `products`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+ALTER TABLE `korpa`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=107;
+
+--
+-- AUTO_INCREMENT for table `proizvodi`
+--
+ALTER TABLE `proizvodi`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=16;
 
 --
 -- AUTO_INCREMENT for table `users`
 --
 ALTER TABLE `users`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=8;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
