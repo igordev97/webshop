9ba7afc478695d96a5091091487ac287bd1c148d
igordev97 <business.igorstojadinovic97@gmail.com>
2024-02-15 22:47:15 +0100

    Add new version of database

--- bazaCopy/webshop.sql
+++ bazaCopy/webshop.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Feb 15, 2024 at 09:25 PM
+-- Generation Time: Feb 15, 2024 at 10:46 PM
 -- Server version: 10.4.32-MariaDB
 -- PHP Version: 8.2.12
 
@@ -37,17 +37,6 @@ CREATE TABLE `korpa` (
   `slika_proizvod` varchar(128) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
---
--- Dumping data for table `korpa`
---
-
-INSERT INTO `korpa` (`id`, `id_korisnik`, `id_proizvod`, `ime_proizvod`, `kolicina_proizvod`, `cena_proizvod`, `slika_proizvod`) VALUES
-(102, 6, 15, 'Kingston DIMM DDR4 16GB 3200MHz', 1, 8500, 'kingston-dimm-ddr4-16gb-3200mhz-kf432c16bb1-16-fury-beast-ram-memorija-cene.jpg'),
-(103, 7, 13, 'Asus Prime B550A-CMS Maticna Ploca', 1, 13000, 'asus-prime-b550m-a-maticna-ploca-6540ea0c7e986.jpeg'),
-(104, 7, 12, 'AMD Ryzen 5 5600x -Box Kuler', 1, 19000, 'ryzen.png'),
-(105, 7, 14, 'AMD RX 6650 XT 8GB DDR6 - Power Color', 1, 31000, 'image64ec8d7ae5881.jpg'),
-(106, 7, 15, 'Kingston DIMM DDR4 16GB 3200MHz', 1, 8500, 'kingston-dimm-ddr4-16gb-3200mhz-kf432c16bb1-16-fury-beast-ram-memorija-cene.jpg');
-
 -- --------------------------------------------------------
 
 --
@@ -70,9 +59,12 @@ CREATE TABLE `proizvodi` (
 
 INSERT INTO `proizvodi` (`id`, `ime_proizvoda`, `opis_proizvoda`, `cena_proizvoda`, `kolicina_proizvoda`, `kategorija_proizvoda`, `slika_proizvoda`) VALUES
 (12, 'AMD Ryzen 5 5600x -Box Kuler', 'Procesor: AMD Ryzen 5 5600x\r\nSoket:AM4\r\n\r\nSavrsen gaming procesor spreman za igranjae', 19000, 99, 'procesori', 'ryzen.png'),
-(13, 'Asus Prime B550A-CMS Maticna Ploca', 'Soket:AM4\r\nRam Support : DDR4\r\nRGB:yes', 13000, 100, 'maticne_ploce', 'asus-prime-b550m-a-maticna-ploca-6540ea0c7e986.jpeg'),
-(14, 'AMD RX 6650 XT 8GB DDR6 - Power Color', 'Savrsena grafika za sve moderne igre.\r\n\r\nSa ovom grafikom mozete igrati sve najnovije naslove na max detaljima u FULL HD rezoluciji', 31000, 100, 'graficke', 'image64ec8d7ae5881.jpg'),
-(15, 'Kingston DIMM DDR4 16GB 3200MHz', '16GB DDR4 brza memorija sve sto vam je potrebno za vase ugodno koriscenje', 8500, 100, 'ram_memorije', 'kingston-dimm-ddr4-16gb-3200mhz-kf432c16bb1-16-fury-beast-ram-memorija-cene.jpg');
+(13, 'Asus Prime B550A-CMS Maticna Ploca', 'Soket:AM4\r\nRam Support : DDR4\r\nRGB:yes', 13000, 99, 'maticne_ploce', 'asus-prime-b550m-a-maticna-ploca-6540ea0c7e986.jpeg'),
+(14, 'AMD RX 6650 XT 8GB DDR6 - Power Color', 'Savrsena grafika za sve moderne igre.\r\n\r\nSa ovom grafikom mozete igrati sve najnovije naslove na max detaljima u FULL HD rezoluciji', 31000, 91, 'graficke', 'image64ec8d7ae5881.jpg'),
+(15, 'Kingston DIMM DDR4 16GB 3200MHz', '16GB DDR4 brza memorija sve sto vam je potrebno za vase ugodno koriscenje', 8500, 0, 'ram_memorije', 'kingston-dimm-ddr4-16gb-3200mhz-kf432c16bb1-16-fury-beast-ram-memorija-cene.jpg'),
+(16, 'Cooler Master V750 750W Fully Modular 80 PLUS Gold Power Supply', 'Cooler Master V750 750W Fully Modular 80 PLUS Gold Power Supply\r\n\r\nSavrseno napajanje za svaki pc build\r\n\r\nNapajanje je snage 750W \r\n\r\n\r\n', 14000, 50, 'napajanje', 'v750_gallery_colorbox-zoom.png'),
+(17, ' Zotac Gaming Geforce RTX 3060 Twin Edge 12GB 192 bit', ' Nvidia Zotac Gaming Geforce RTX 3060 Twin Edge 12GB 192 bit\r\n\r\nSavrsena grafika za Full Hd gaming iz Nvidie \r\n\r\nDolazi sa RTX funkcijom koja omogucava raytracing', 36000, 100, 'graficke', '77583.jpg'),
+(18, 'NZXT H5 flow kućište belo', 'Predstavljamo NZXT H5 flow kućište – savršen spoj performansi, stila i funkcionalnosti. Ovo elegantno i moderno kućište Midi Tower je dizajnirano da prihvati niz formata matičnih ploča, uključujući ATX, EATX (do 272 mm), microATX i MiniITX, dajući vam fleksibilnost da napravite svoj računar iz snova.\r\n\r\nNZXT H5 flow kućište belo dolazi bez napajanja, što vam omogućava da odaberete savršenu jedinicu za napajanje za vaše specifične potrebe. Sa 1 internim ležištem za disk jedinice od 3,5\" i 2 interna ležišta za disk jedinice od 2,5\" (konvertibilno sa 1 odeljka od 3,5\"), imaćete dovoljno prostora za skladištenje svih vaših datoteka i igara.', 11990, 0, 'kucista', 'nzxt-h5-flow-kuciste-belo-cc-h51fw-01-cene.jpg');
 
 -- --------------------------------------------------------
 
@@ -130,13 +122,13 @@ ALTER TABLE `users`
 -- AUTO_INCREMENT for table `korpa`
 --
 ALTER TABLE `korpa`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=107;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=129;
 
 --
 -- AUTO_INCREMENT for table `proizvodi`
 --
 ALTER TABLE `proizvodi`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=16;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=19;
 
 --
 -- AUTO_INCREMENT for table `users`
